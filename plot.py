dacc438bf3506fde89e339f88eac22998e8a73cd
Bolado <vtcb@cin.ufpe.br>
2017-10-15 00:38:19 -0300

    Plot multiple plots

--- plot.py
+++ plot.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 algos = [
@@ -11,20 +10,43 @@ algos = [
   ('ukkonen', True)
 ]
 
-results_per_alg = {}
-with open('stats/results.txt') as results:
-  for line in results:
-    if not line.strip(): continue
-    pat, txt, err, alg, tim = line.split()
-    # print(tim);
-    test = (pat, txt, err);
-    
-    if alg not in results_per_alg:
-      results_per_alg[alg] = []
-    results_per_alg[alg].append(tim)
+def isApprox(alg):
+  return alg in [alg for alg, isa in algos if isa]
 
-for alg in results_per_alg:
-  results = results_per_alg[alg]
-  plt.plot(results)
-plt.show()
+def plot(results_per_alg, title, fig_name):
+  fig, ax = plt.subplots()
+  for alg in results_per_alg:
+    results = results_per_alg[alg]
+    ax.plot(results[0], results[1], '-x', label=alg)
+  ax.set_xlabel('Pattern(s) size')
+  ax.set_ylabel('Execution time')
 
+  ax.legend(loc='best', fancybox=True, framealpha=0.5)
+  fig.suptitle(title)
+
+  fig.savefig(fig_name)
+  plt.close(fig)
+
+def getResults():
+  results_per_plt = {}
+  with open('stats/results.txt') as results_file:
+    for line in results_file:
+      if not line.strip(): continue
+      pid, siz, err, alg, tim = line.split()
+      siz = int(siz)
+      err = int(err)
+      tim = float(tim)
+
+      if pid not in results_per_plt:
+        results_per_plt[pid] = {}
+      if alg not in results_per_plt[pid]:
+        results_per_plt[pid][alg] = [[], []]
+
+      results_per_plt[pid][alg][0].append(siz)
+      results_per_plt[pid][alg][1].append(tim)
+  return results_per_plt
+
+results = getResults()
+
+for pid in results:
+  plot(results[pid], pid, '{}.png'.format(pid))
