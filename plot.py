0b8d16b69cc1dcefcca14ab0302bd45826517ab0
Bolado <vtcb@cin.ufpe.br>
2017-10-15 00:56:23 -0300

    Fix order

--- plot.py
+++ plot.py
@@ -1,28 +1,20 @@
+from os import system
 import matplotlib.pyplot as plt
 
-algos = [
-  ('naive',   False),
-  ('kmp',     False),
-  ('aho',     False),
-  ('so',      False),
-  ('bm',      False),
-  ('sellers', True),
-  ('ukkonen', True)
-]
+def sortByFirst(a, b):
+  return zip(*sorted(zip(a, b)))
 
-def isApprox(alg):
-  return alg in [alg for alg, isa in algos if isa]
-
-def plot(results_per_alg, title, fig_name):
+def plot(results_per_alg, pid, fig_name):
   fig, ax = plt.subplots()
   for alg in results_per_alg:
-    results = results_per_alg[alg]
-    ax.plot(results[0], results[1], '-x', label=alg)
+    x, y = results_per_alg[alg]
+    x, y = sortByFirst(x, y)
+    ax.plot(x, y, '-x', label=alg)
   ax.set_xlabel('Pattern(s) size')
   ax.set_ylabel('Execution time')
 
   ax.legend(loc='best', fancybox=True, framealpha=0.5)
-  fig.suptitle(title)
+  # fig.suptitle(pid)
 
   fig.savefig(fig_name)
   plt.close(fig)
@@ -46,7 +38,11 @@ def getResults():
       results_per_plt[pid][alg][1].append(tim)
   return results_per_plt
 
-results = getResults()
+def main():
+  results = getResults()
 
-for pid in results:
-  plot(results[pid], pid, '{}.png'.format(pid))
+  system('mkdir -p figs')
+  for pid in results:
+    plot(results[pid], pid, 'figs/{}.png'.format(pid))
+
+if __name__ == '__main__': main()
